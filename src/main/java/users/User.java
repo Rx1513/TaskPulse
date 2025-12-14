46149148b5c8dd6f43fe44aa9505f9336205c426
Rx1513 <rx1513@altlinux.org>
2025-12-14 17:44:33 +0300

    Add lombok

--- src/main/java/users/User.java
+++ src/main/java/users/User.java
@@ -1,25 +1,12 @@
 package users;
 
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@AllArgsConstructor
 public class User {
     private final long id;
     private final String name;
     private final String email;
-
-    public User(long id,String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
