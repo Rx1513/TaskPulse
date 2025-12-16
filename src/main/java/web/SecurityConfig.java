45da96640209b58760155f7282d54fcbb1e9861c
Un1u6ky <dudarev_danya@mail.ru>
2025-12-16 17:35:20 +0300

    Добавлено оформление для страниц, pom.xml возвращен с ветки master, убраны csrf токены и SpringSecurity на время

--- src/main/java/web/SecurityConfig.java
+++ src/main/java/web/SecurityConfig.java
@@ -1,4 +1,4 @@
-package web;
+/*package web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,4 +42,4 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(user);
     }
 
-}
+}*/
