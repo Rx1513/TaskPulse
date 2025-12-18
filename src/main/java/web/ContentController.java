7c00dc7dd4eccbe5987c51710c01bb49dd6803e4
Rx1513 <rx1513@altlinux.org>
2025-12-19 02:24:30 +0300

    Finish merging pages with content controller

--- src/main/java/web/ContentController.java
+++ src/main/java/web/ContentController.java
@@ -1,20 +1,30 @@
 package web;
 
+import database.TaskRepository;
+import database.UserRepository;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
 
 import tasks.Task;
+import users.User;
 
 @Controller
 public class ContentController {
 
-    private final List<Task> tasks = new ArrayList<>();
-    private String currentUser = null;
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    TaskRepository taskRepository;
 
     @GetMapping("/auth/login")
     public ModelAndView login() {
@@ -29,7 +39,11 @@ public class ContentController {
     @GetMapping(path = { "/tasks", "/" })
     public ModelAndView tasks(Principal principal) {
         ModelAndView mv = new ModelAndView("tasks");
-        mv.addObject("tasks", tasks);
+        Optional<User> curerentUser = userRepository.findUserByName(principal.getName());
+        if (curerentUser.isEmpty()) {
+            throw new EmptyResultDataAccessException("Текущий авторизованный пользователь не найден! " + principal.getName(), 1);
+        }
+        mv.addObject("tasks", taskRepository.getTasksPreviewsByUser(curerentUser.get()));
         mv.addObject("currentUser", principal.getName());
         return mv;
     }
@@ -42,30 +56,26 @@ public class ContentController {
     }
 
     @GetMapping("/task/show/{id}")
-    public ModelAndView showTask(@PathVariable int id) {
-        return tasks.stream()
-                .filter(t -> t.getId() == id)
-                .findFirst()
-                .map(task -> {
-                    ModelAndView mv = new ModelAndView("show_task");
-                    mv.addObject("task", task);
-                    mv.addObject("currentUser", currentUser);
-                    return mv;
-                })
-                .orElseGet(() -> new ModelAndView("redirect:/tasks"));
+    public ModelAndView showTask(@PathVariable int id, Principal principal) {
+        ModelAndView mv = new ModelAndView("show_task");
+        mv.addObject("currentUser", principal.getName());
+        Optional<Task> task = taskRepository.findTaskById(id);
+        if (task.isEmpty()) {
+            return new ModelAndView("redirect:/tasks");
+        }
+        mv.addObject("task", task.get());
+        return mv;
     }
 
     @GetMapping("/task/edit/{id}")
-    public ModelAndView editTaskForm(@PathVariable int id) {
-        return tasks.stream()
-                .filter(t -> t.getId() == id)
-                .findFirst()
-                .map(task -> {
-                    ModelAndView mv = new ModelAndView("edit_task");
-                    mv.addObject("task", task);
-                    mv.addObject("currentUser", currentUser);
-                    return mv;
-                })
-                .orElseGet(() -> new ModelAndView("redirect:/tasks"));
+    public ModelAndView editTaskForm(@PathVariable int id, Principal principal) {
+        ModelAndView mv = new ModelAndView("edit_task");
+        mv.addObject("currentUser", principal.getName());
+        Optional<Task> task = taskRepository.findTaskById(id);
+        if (task.isEmpty()) {
+            return new ModelAndView("redirect:/tasks");
+        }
+        mv.addObject("task", task.get());
+        return mv;
     }
 }
